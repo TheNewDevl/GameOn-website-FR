570942b5fa34ebe84adc7c5efdfd38b433f88867
carlos <carl.devfs@gmail.com>
2022-10-13 11:12:59 +0200

    #2 form issue (1) add js inputs validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -35,3 +35,57 @@ const closeModal = () => {
 };
 
 closeModal();
+
+const form = document.querySelector("form");
+
+const validators = {
+  firstName(input) {
+    return /^([a-zA-Z'éèàçêï\+-][\s]{0,1}){2,30}$/.test(input.value);
+  },
+  lastName(input) {
+    return /^([a-zA-Z'éèàçêï\+-][\s]{0,1}){2,30}$/.test(input.value);
+  },
+  email(input) {
+    return /^([a-z0-9]+(?:[._-][a-z0-9]+)*)@([a-z0-9]+(?:[.-][a-z0-9]+)*\.[a-z]{2,})$/i.test(
+      input.value
+    );
+  },
+  birthdate(input) {
+    const inputDate = new Date(input.value);
+    if (!inputDate.getDate()) return false;
+    const diff = Date.now() - inputDate;
+    const age = Math.abs(new Date(diff).getUTCFullYear() - 1970);
+    return age >= 18;
+  },
+  quantity(input) {
+    return +input.value > 0 && +input.value < 100;
+  },
+};
+
+/** Check all inputs validity */
+const checkFormValidity = (e) => {
+  let formValidity = true;
+
+  const inputs = document.querySelectorAll(
+    "#firstName,#lastName,#email,#birthdate,#quantity"
+  );
+  const requiredCheckbox = document.querySelector("#checkbox1");
+  const checkedLocation = document.querySelectorAll("[name=location]:checked");
+
+  inputs.forEach((input) => {
+    if (!validators[input.id](input)) {
+      formValidity = false;
+    }
+  });
+  if (!requiredCheckbox.checked) formValidity = false;
+  if (checkedLocation.length !== 1) formValidity = false;
+
+  if (!formValidity) {
+    e.preventDefault();
+  } else {
+    console.log("Il y a une erreur dans le formulaire");
+  }
+  return formValidity;
+};
+
+form.addEventListener("submit", checkFormValidity);
